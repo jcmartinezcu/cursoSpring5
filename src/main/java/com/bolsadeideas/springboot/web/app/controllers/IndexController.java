e11c3100e973f8cb4c0af301e4e6c5ea56d9a0c3
jcmartinez <jcmartinezcu@gmail.com>
2019-12-17 20:47:22 +0100

    video 25

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,9 +1,9 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +15,19 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
+	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
 
 	@GetMapping({"/index", "/", "","/home"})
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring frameWork con Model!!");
+		model.addAttribute("titulo", textoIndex);
 		return "index";
 		
 	}
@@ -32,7 +41,7 @@ public class IndexController {
 		usuario.setEmail("andres@correo.com");
 		
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 		
 		return "perfil";		
 	}
@@ -40,7 +49,7 @@ public class IndexController {
 	@RequestMapping("/listar")
 	public String listar(Model model) {
 		
-		model.addAttribute("titulo", "listado de usuarios");
+		model.addAttribute("titulo", textoListar);
 		
 		return "listar";
 	}
